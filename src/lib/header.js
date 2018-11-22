bfe437f5f6084badaf5ea2b200a78fc9bc98289e
Þröstur A <thth168@hi.is>
2018-11-22 22:59:02 +0000

    javaascreipty

--- src/lib/header.js
+++ src/lib/header.js
@@ -12,15 +12,18 @@ export default class Header {
 
   load() {
     empty(this.container);
+    this.containerAttributes = [newAttribute('class', `${this.parentClass}__cointainer`)];
+    this.divElement = createElement('div', '', this.containerAttributes);
     this.categoryAttr = [newAttribute('class', `${this.parentClass}__category`)];
     this.categoryElement = createElement('p', this.category, this.categoryAttr);
     this.titleAttr = [newAttribute('class', `${this.parentClass}__title`)];
     this.titleElement = createElement('h1', this.title, this.titleAttr);
     this.imageAttr = [newAttribute('src', this.imagepath), newAttribute('class', `${this.parentClass}__image`)];
     this.imageElement = createElement('img', '', this.imageAttr);
+    this.divElement.appendChild(this.categoryElement);
+    this.divElement.appendChild(this.titleElement);
 
-    this.container.appendChild(this.categoryElement);
-    this.container.appendChild(this.titleElement);
+    this.container.appendChild(this.divElement);
     this.container.appendChild(this.imageElement);
   }
 }
